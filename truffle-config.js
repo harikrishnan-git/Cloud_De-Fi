2f7ca247da9eb93c9c639351ab28dafb207d67c4
harikrishnan-git <haribkrishh@gmail.com>
2025-04-09 17:16:22 +0530

    added abis to gitignore

--- truffle-config.js
+++ truffle-config.js
@@ -5,11 +5,11 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const { version } = require("react");
 module.exports = {
   networks: {
-    // development: {
-    //   host: "127.0.0.1",
-    //   port: 7545,
-    //   network_id: "*", // Match any network id
-    // },
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+    },
     sepolia: {
       provider: () =>
         new HDWalletProvider({
